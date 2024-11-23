f8e394d03a9d752f28fd5cb0ad7f4be5083bc0b3
Nicole Hollant <nicolehollant@gmail.com>
2024-11-22 20:16:05 -0500

    ugh

--- src/app.vue
+++ src/app.vue
@@ -7,11 +7,14 @@
         </NuxtLayout>
       </TooltipProvider>
       <Toaster />
+      <Analytics></Analytics>
     </div>
   </ConfigProvider>
 </template>
 
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+
 import { ConfigProvider } from 'radix-vue'
 const useIdFunction = () => useId()
 </script>
